7c83b7e9abf0b10b7ad27214468572ccd240fca6
Jordan Lui <jordan.lui@gmail.com>
2017-03-24 22:04:21 +0100

    added more pages

--- app.rb
+++ app.rb
@@ -3,3 +3,22 @@ require "sinatra"
 get "/" do
 	"HELLO WORLD"
 end
+
+get "/about" do
+	"This is Jordan's first Ruby app on Heroku. Feeling good"
+end
+
+get "/cats" do
+	"This is a page about cats"
+end
+
+get "/space" do
+	"So you like space stuff? We should put a photo here"
+end
+
+get "/square/:x" do
+	x = params[:x].to_i
+	y = x*x
+	"The square of your number is #{y}"
+	
+end
