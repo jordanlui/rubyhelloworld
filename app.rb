6a87358f75692c5e9c3a58d58164850ef3aa5500
Jordan Lui <jordan.lui@gmail.com>
2017-03-25 00:00:21 +0100

    added views for basic erb

--- app.rb
+++ app.rb
@@ -1,18 +1,34 @@
 require "sinatra"
+require "open-uri"
+require "json"
 
 get "/" do
-	"HELLO WORLD"
+	# "HELLO WORLD"
+	erb :home
 end
 
 get "/about" do
 	"This is Jordan's first Ruby app on Heroku. Feeling good"
 end
 
+get "/api" do
+	response = open('https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo').read
+	response = JSON.parse(response)
+	url = response["url"]
+
+	"API PAGE. Result is #{response["url"]}"
+end
+
+
+
 get "/cats" do
+	# inline cat image HTML tag?
 	"This is a page about cats"
+
 end
 
 get "/cat" do
+	# This is how we link out to an HTL page
 	send_file "cat.html"
 end
 
